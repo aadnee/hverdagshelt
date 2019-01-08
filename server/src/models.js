f5df28c4dece6ed8afb752367fc16ac63020e486
Mathias Knutsen <mathiakn@stud.ntnu.no>
2019-01-08 16:20:16 +0100

    DB models

--- server/src/models.js
+++ server/src/models.js
@@ -22,7 +22,7 @@ export let Users = sequelize.define('users', {
   rank: Sequelize.INTEGER
 });
 
-export let CompanyUsers = sequelize.define('companyUsers', {
+export let Companies = sequelize.define('companies', {
   id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   name: Sequelize.STRING,
   email: Sequelize.STRING,
@@ -30,35 +30,92 @@ export let CompanyUsers = sequelize.define('companyUsers', {
   password: Sequelize.STRING
 });
 
-export let Municipals = sequelize.define('municipal', {
+export let Municipals = sequelize.define('municipals', {
   id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
   name: Sequelize.STRING,
   postalCode: Sequelize.INTEGER
 });
 
-Users.belongsTo(Municipals);
-CompanyUsers.belongsTo(Municipals);
+export let Categories = sequelize.define('categories', {
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  name: Sequelize.STRING
+});
 
-// Drop tables and create test data when not in production environment
+export let Tickets = sequelize.define('tickets', {
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  title: Sequelize.STRING,
+  description: { type: Sequelize.TEXT, length: 'medium' },
+  status: Sequelize.INTEGER,
+  lat: Sequelize.FLOAT,
+  lon: Sequelize.FLOAT
+});
+
+export let News = sequelize.define('news', {
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  title: Sequelize.STRING,
+  description: { type: Sequelize.TEXT, length: 'medium' },
+  status: Sequelize.INTEGER,
+  lat: Sequelize.FLOAT,
+  lon: Sequelize.FLOAT
+});
+
+export let Subscriptions = sequelize.define('subscriptions');
+News.belongsToMany(Users, { through: Subscriptions });
+Users.belongsToMany(News, { through: Subscriptions });
+
+Tickets.belongsTo(Users);
+Users.belongsTo(Municipals);
+Companies.belongsTo(Municipals);
+Categories.belongsTo(Categories, { as: 'parent' });
+Tickets.belongsTo(Categories);
+News.belongsTo(Categories);
+
 let production = process.env.NODE_ENV === 'production';
-// The sync promise can be used to wait for the database to be ready (for instance in your tests)
 export let sync = sequelize.sync({ force: production ? false : true }).then(() => {
-  if (!production)
-    return Users.create({
+  if (!production) {
+    Municipals.create({
+      name: 'Lindesnes',
+      postalCode: 4520
+    });
+    Categories.create({
+      name: 'Vei og trafikk'
+    });
+    Categories.create({
+      name: 'Setting av brøytestikker',
+      parentId: 1
+    });
+    Users.create({
       firstName: 'Ola',
       lastName: 'Jensen',
       email: 'ola.jensen@ntnu.no',
       phone: 123,
       password: '123',
-      rank: 3
-    }).then(() =>
-      Users.create({
-        firstName: 'Kari',
-        lastName: 'Larsen',
-        email: 'kari.larsen@ntnu.no',
-        phone: 123,
-        password: '12345',
-        rank: 1
-      })
-    );
+      rank: 3,
+      municipalId: 1
+    });
+    Companies.create({
+      name: 'SmartPark',
+      email: 'admin@smartpark.no',
+      phone: 12345678,
+      password: '12345',
+      municipalId: 1
+    });
+    Tickets.create({
+      title: 'Vei problem',
+      description: 'Pls sett opp brøytestikker her.',
+      status: 3,
+      categoryId: 2
+    });
+    News.create({
+      title: 'Problem ved vei i TRD sentrum,.',
+      description: 'Brøytestikker skal bli satt opp.',
+      status: 1,
+      categoryId: 2
+    });
+    Subscriptions.create({
+      newsId: 1,
+      userId: 1
+    });
+    return true;
+  }
 });
