03c2ccdfed4a9b65edc659a0b9137202120ab983
Ådne Eide Stavseng <aadnees@stud.ntnu.no>
2019-01-09 16:36:25 +0100

    Map implementation v1.0

--- client/src/js/AppRouter.js
+++ client/src/js/AppRouter.js
@@ -26,7 +26,7 @@ import {
 } from './pages/files';
 
 //Edit MyWidget inside curlybrackets and in the file link.
-import { FooterWidget as Widget } from './widgets/FooterWidget';
+import {MapWidget as Widget} from './widgets/MapWidget';
 
 
 export class AppRouter extends Component {
@@ -61,7 +61,7 @@ export class AppRouter extends Component {
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/logout" component={LogoutPage} />
           {/* Develoopment */}
-          <Route exact path="/widget" component={Widget} />
+          <Route exact path="/map" component={Widget} />
         </div>
       </HashRouter>
     );
