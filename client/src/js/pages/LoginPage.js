4f229226f28215f3c4f30cbe813bad934658dca5
fredrmed <fredrmed@stud.ntnu.no>
2019-01-09 15:58:32 +0100

    Logos

--- client/src/js/pages/LoginPage.js
+++ client/src/js/pages/LoginPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import { Container, Grid, Header, Image, Segment, Form, Message, Button } from 'semantic-ui-react';
 
 //import {} from './../widgets';
 
@@ -9,7 +9,24 @@ export class LoginPage extends Component {
   render() {
     return (
       <Container>
-        <p>Text</p>
+        <Grid textAlign="center" verticalAlign="middle">
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Image src="img/vector-logo-lav-farge.png" />
+            <Form size="large">
+              <Segment stacked>
+                <Form.Input fluid icon="user" iconPosition="left" placeholder="E-postadresse" />
+                <Form.Input fluid icon="lock" iconPosition="left" placeholder="Passord" type="password" />
+
+                <Button color="blue" fluid size="large">
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+            <Message>
+              New to us? <a href="#">Sign Up</a>
+            </Message>
+          </Grid.Column>
+        </Grid>
       </Container>
     );
   }
