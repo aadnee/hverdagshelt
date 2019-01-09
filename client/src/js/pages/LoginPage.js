0dfc10077339a1b9bd41d7c8ddca3b985fc8d07d
fredrmed <fredrmed@stud.ntnu.no>
2019-01-09 16:22:15 +0100

    Design LoginPage Done, Needs logic

--- client/src/js/pages/LoginPage.js
+++ client/src/js/pages/LoginPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Container, Grid, Header, Image, Segment, Form, Message, Button } from 'semantic-ui-react';
+import { Container, Grid, Header, Image, Segment, Form, Message, Button, Input } from 'semantic-ui-react';
 
 //import {} from './../widgets';
 
@@ -9,21 +9,27 @@ export class LoginPage extends Component {
   render() {
     return (
       <Container>
-        <Grid textAlign="center" verticalAlign="middle">
+        <Grid verticalAlign="middle">
           <Grid.Column style={{ maxWidth: 450 }}>
             <Image src="img/vector-logo-lav-farge.png" />
             <Form size="large">
-              <Segment stacked>
-                <Form.Input fluid icon="user" iconPosition="left" placeholder="E-postadresse" />
-                <Form.Input fluid icon="lock" iconPosition="left" placeholder="Passord" type="password" />
+              <Segment piled>
+                <Form.Field>
+                  <label>E-postadresse</label>
+                  <Input fluid icon="user" iconPosition="left" placeholder="E-postadresse" />
+                </Form.Field>
+                <Form.Field>
+                  <label>Passord</label>
+                  <Input fluid icon="lock" iconPosition="left" placeholder="Passord" type="password" />
+                </Form.Field>
 
                 <Button color="blue" fluid size="large">
-                  Login
+                  Logg inn
                 </Button>
               </Segment>
             </Form>
             <Message>
-              New to us? <a href="#">Sign Up</a>
+              Har du ikke bruker? <NavLink to="/register">Registrer deg</NavLink>
             </Message>
           </Grid.Column>
         </Grid>
