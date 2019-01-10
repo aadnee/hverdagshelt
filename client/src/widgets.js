2c7612923a85d72f380479024f03a9472ffb197d
Eivind Alfsvåg Johansen <eivindaj@stud.ntnu.no>
2019-01-10 08:33:34 +0100

    Merge branch 'master' of C:\Users\Eivind A. Johansen\hverdagshelt with conflicts.

--- client/src/widgets.js
+++ client/src/widgets.js
@@ -1,4 +1,72 @@
 import * as React from 'react';
+import { Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse, FormInline, Dropdown, DropdownToggle, DropdownMenu,  DropdownItem, Fa } from "mdbreact";
+
+
+export class HeaderMain extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+
+        <Navbar color="default-color" dark expand="md" style={{marginTop: "20px"}}>
+          <NavbarBrand>
+            <strong className="white-text">Hverdagshelt</strong>
+          </NavbarBrand>
+          <NavbarToggler
+              onClick={this.toggleCollapse}
+          />
+          <Collapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+            <NavbarNav left>
+              <NavItem active>
+                <NavLink to="#!">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="#!">Features</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="#!">Pricing</NavLink>
+              </NavItem>
+              <NavItem>
+                <Dropdown>
+                  <DropdownToggle  nav caret>
+                    <div className="d-none d-md-inline">Dropdown</div>
+                  </DropdownToggle>
+                  <DropdownMenu className="dropdown-default"right>
+                    <DropdownItem href="#!">Action</DropdownItem>
+                    <DropdownItem href="#!">Another Action</DropdownItem>
+                    <DropdownItem href="#!">Something else here</DropdownItem>
+                    <DropdownItem href="#!">Something else here</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </NavItem>
+            </NavbarNav>
+            <NavbarNav right>
+              <NavItem>
+                <Dropdown>
+                  <DropdownToggle nav caret>
+                    <Fa icon="user" />
+                  </DropdownToggle>
+                  <DropdownMenu className="dropdown-default" right>
+                    <DropdownItem href="#!">Min Side</DropdownItem>
+                    <DropdownItem href="#!">Another Action</DropdownItem>
+                    <DropdownItem href="#!">Something else here</DropdownItem>
+                    <DropdownItem href="#!">Something else here</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </NavItem>
+            </NavbarNav>
+          </Collapse>
+        </Navbar>
+    );
+  }
+}
 
 export class Alert extends React.Component {
   alerts = [];
