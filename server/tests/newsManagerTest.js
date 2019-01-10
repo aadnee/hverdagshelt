00991e61fa23d4c5664cb0471d69b50227afb797
Terje Haugum <terhaug@stud.ntnu.no>
2019-01-10 09:57:12 +0100

    Adding new test

---
+++ server/tests/newsManagerTest.js
@@ -0,0 +1,35 @@
+import { News, sync } from '../src/models.js';
+import { newsManager } from '../src/managers/newsManager.js';
+
+beforeAll(async () => {
+  await sync;
+  const id = newsManager.addArticle('TestArticle', 'Dette er en test som skal funke', 1, 1, 1.123, 2.234, 1);
+
+
+// Testing adding a new article
+describe('Adding article', () => {
+  it('correct data', async () => {
+    let news = await newsManager.addArticle(title, description, status, categoryId, lat, lon, municipalId)
+      {
+
+    expect({
+      title: news.title,
+      description: news.description,
+      status: news.status,
+      categoryId: news.categoryId,
+      lat: news.lat,
+      lon: news.lon,
+      municipalId: news.municipalId
+    }).toEqual(
+      {
+        title: 'TestArticle',
+        description: 'Dette er en test som skal funke',
+        status: 1,
+        categoryId: 1,
+        lat: 1.123,
+        lon: 2.234
+      }
+    );
+  };
+});
+});
