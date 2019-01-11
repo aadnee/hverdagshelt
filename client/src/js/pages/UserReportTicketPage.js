ea5c5265e112d8cc7f82fe3e81ff32fb4a3ab12c
sigurlj <sigurlj@stud.ntnu.no>
2019-01-11 11:34:50 +0100

    connected ticked widgets to backand and services

--- client/src/js/pages/UserReportTicketPage.js
+++ client/src/js/pages/UserReportTicketPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-
+import {Component} from 'react';
+import {NavLink} from 'react-router-dom';
+import {Header, Modal} from 'semantic-ui-react';
 import {TicketFormWidget} from '../widgets/TicketFormWidget';
 
 export class UserReportTicketPage extends Component {
-  render() {
-    return(
-        <TicketFormWidget/>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Header size={'huge'} textAlign={'center'}>Meld inn feil</Header>
+                <TicketFormWidget/>
+            </div>
+        )
+    }
 }
