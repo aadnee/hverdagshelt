e8f4adb66b04653f78260423eef45e3fc625ecaa
unknown <eivinhl@stud.ntnu.no>
2019-01-11 10:06:47 +0100

    Prettier and tests

--- server/tests/newsManagerTest.js
+++ server/tests/newsManagerTest.js
@@ -8,34 +8,31 @@ beforeAll(async () => {
   //id = await newsManager.addArticle('TestArticle', 'Dette er en test som skal funke', 1, 1, 1.123, 2.234, 1);
 });
 
-
-
 // Testing adding a new article
 describe('Adding article', () => {
   it('correct data', async () => {
     //let news = await newsManager.addArticle(title, description, status, categoryId, lat, lon, municipalId)
-    let article = newsManager.addArticle('TestArticle', 'Dette er en test som skal funke', 1, 1, 1.123, 2.234, 1);
-    console.log('adding article - test');
-    let news = await News.findOne({ where: { id: article.id }});
-    {
-      expect({
-        title: news.title,
-        description: news.description,
-        status: news.status,
-        categoryId: news.categoryId,
-        lat: news.lat,
-        lon: news.lon,
-        municipalId: news.municipalId
-      }).toEqual({
-        title: 'TestArticle',
-        description: 'Dette er en test som skal funke',
-        status: 1,
-        categoryId: 1,
-        lat: 1.123,
-        lon: 2.234,
-        municipalId: 1
+    newsManager.addArticle('TestArticle', 'Dette er en test som skal funke', 1, 1, 1.123, 2.234, 1, function(article) {
+      console.log('adding article - test');
+      News.findOne({ where: { id: article.id } }).then(news => {
+        expect({
+          title: news.title,
+          description: news.description,
+          status: news.status,
+          categoryId: news.categoryId,
+          lat: news.lat,
+          lon: news.lon,
+          municipalId: news.municipalId
+        }).toEqual({
+          title: 'TestArticle',
+          description: 'Dette er en test som skal funke',
+          status: 1,
+          categoryId: 1,
+          lat: 1.123,
+          lon: 2.234,
+          municipalId: 1
+        });
       });
-    }
+    });
   });
 });
-
