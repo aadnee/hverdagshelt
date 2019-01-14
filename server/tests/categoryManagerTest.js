7aaf784eee8c387b2f1d11fc65d7f16687f38c56
Eivind Hest <eivinhl@stud.ntnu.no>
2019-01-14 13:08:38 +0100

    Added tests for the methods in categoryManager.

---
+++ server/tests/categoryManagerTest.js
@@ -0,0 +1,46 @@
+import { Users, sync } from '../src/models';
+import categoryManager from '../src/managers/categoryManager';
+
+jest.setTimeout(30000);
+
+beforeAll(async () => {
+  await sync;
+});
+
+describe('CategoryManager tests', () => {
+  it('correct data', done => {
+    categoryManager.getCategories(function(result) {
+      expect({
+        success: result.success,
+        name: result.data[0].name
+      }).toEqual({
+        success: true,
+        name: 'Vei og trafikk'
+      });
+      done();
+    });
+  });
+
+  it('correct data', done => {
+    categoryManager.addCategory('Skog og utmark', null, function(result) {
+      expect({
+        success: result.success
+      }).toEqual({
+        success: true
+      });
+      done();
+    });
+  });
+  it('correct data', done => {
+    categoryManager.getSubCategories(1, function(result) {
+      expect({
+        success: result.success,
+        name: result.data[0].name
+      }).toEqual({
+        success: true,
+        name: 'Setting av brøytestikker'
+      });
+      done();
+    });
+  });
+});
