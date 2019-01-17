e2b22583000ba7984683116e4ac7aafa05afb25d
Mona Ullah <monaul@stud.ntnu.no>
2019-01-17 10:01:24 +0100

    minor changes design

--- client/src/js/pages/UserTicketsPage.js
+++ client/src/js/pages/UserTicketsPage.js
@@ -1,48 +1,50 @@
 import React from 'react';
-import { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { Grid, Header, Container, Modal } from 'semantic-ui-react';
-import { TicketWidget } from '../widgets/TicketWidget';
-import { ticketService } from '../services/TicketServices';
-import { TicketFormWidget } from '../widgets/TicketFormWidget';
+import {Component} from 'react';
+import {NavLink} from 'react-router-dom';
+import {Grid, Header, Container, Modal, Segment} from 'semantic-ui-react';
+import {TicketWidget} from '../widgets/TicketWidget';
+import {ticketService} from '../services/TicketServices';
+import {TicketFormWidget} from '../widgets/TicketFormWidget';
 
 export class UserTicketsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showEditTicket: false,
-      tickets: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            showEditTicket: false,
+            tickets: []
+        };
+    }
+
+    componentWillMount() {
+        ticketService.getTickets().then(res => {
+            console.log(res);
+            this.setState({tickets: res.data});
+        });
+    }
+
+    handleEdit = () => {
+        ticketService.UpdateTicket();
     };
-  }
 
-  componentWillMount() {
-    ticketService.getTickets().then(res => {
-      console.log(res);
-      this.setState({ tickets: res.data });
-    });
-  }
-
-  handleEdit = () => {
-    ticketService.UpdateTicket();
-  };
-
-  render() {
-    return (
-      <div>
-        <Container>
-          <Header as="h2">Mine varslinger</Header>
-          <Grid stackable container columns={3}>
-            {this.state.tickets.map(ticket => (
-              <Grid.Column key={ticket.id}>
-                <TicketWidget ticket={ticket} />
-              </Grid.Column>
-            ))}
-          </Grid>
-        </Container>
-        <Modal open={this.state.showEditTicket}>
-          <TicketFormWidget submitButton={'Lagre endringer'} />
-        </Modal>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Header as="h2">Mine varslinger</Header>
+                    <Segment basic color="blue">
+                        <Grid stackable container columns={3}>
+                            {this.state.tickets.map(ticket => (
+                                <Grid.Column key={ticket.id}>
+                                    <TicketWidget ticket={ticket}/>
+                                </Grid.Column>
+                            ))}
+                        </Grid>
+                    </Segment>
+                </Container>
+                <Modal open={this.state.showEditTicket}>
+                    <TicketFormWidget submitButton={'Lagre endringer'}/>
+                </Modal>
+            </div>
+        );
+    }
 }
