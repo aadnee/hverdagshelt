a032f152ee28f01f1f4141715afc4a0561a29842
Mathias Knutsen <mathiakn@stud.ntnu.no>
2019-01-17 15:00:42 +0100

    Added pagination for newsfeed.

--- client/src/js/services/NewsServices.js
+++ client/src/js/services/NewsServices.js
@@ -23,10 +23,11 @@ class NewsService {
     });
   }
 
-  getFilteredNews(municipalIds, categoryIds) {
+  getFilteredNews(municipalIds, categoryIds, page) {
     return api.post('/api/news/filter', {
       municipalIds: municipalIds,
-      categoryIds: categoryIds
+      categoryIds: categoryIds,
+      page: page
     });
   }
 }
