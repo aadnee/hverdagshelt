35d52f67fedcd9297efb5d469b3f22d5c5ee6dbd
fredrmed <fredrmed@stud.ntnu.no>
2019-01-23 09:24:46 +0100

    map update for events

--- client/src/js/widgets/ShowInMapWidget.js
+++ client/src/js/widgets/ShowInMapWidget.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import L from 'leaflet';
 import * as ELG from 'esri-leaflet-geocoder';
 import 'esri-leaflet';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, Marker, Polygon, Popup, TileLayer } from 'react-leaflet';
 import { Button, Icon, Modal } from 'semantic-ui-react';
 import { TicketFormWidget } from '../widgets/TicketFormWidget';
 import { toast } from 'react-toastify';
@@ -17,18 +17,58 @@ export class ShowInMapWidget extends Component {
   }
 
   render() {
-    return (
-      <Modal trigger={this.props.button} onClose={this.props.callback} closeIcon>
-        {this.props.renderMap ? (
-          <Map ref={this.mapRef} dragging={false} center={this.props.latlng} zoom={14}>
-            <TileLayer
-              url="https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}"
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <Marker position={this.props.latlng} />
-          </Map>
-        ) : null}
-      </Modal>
-    );
+    const la = this.props.latlng.reduce((lat, latn) => {
+      console.log(lat);
+      console.log(latn);
+      return [lat[0] + latn[0], lat[1] + latn[1]];
+    });
+
+    const cent = [la[0] / this.props.latlng.length, la[1] / this.props.latlng.length];
+    if (this.props.mapOnly) {
+      return (
+        <Map
+          ref={this.mapRef}
+          dragging={false}
+          center={cent}
+          zoomControl={false}
+          scrollWheelZoom={false}
+          zoom={15}
+          style={{ height: '200px', width: '200px' }}
+        >
+          <TileLayer
+            url="https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {this.props.latlng.length > 1 ? (
+            <Polygon positions={this.props.latlng} />
+          ) : (
+            <Marker position={this.props.latlng[0]} />
+          )}
+        </Map>
+      );
+    } else {
+      return (
+        <Modal trigger={this.props.button} onClose={this.props.callback} closeIcon>
+          {this.props.renderMap ? (
+            <Map
+              ref={this.mapRef}
+              dragging={this.props.draggable ? true : false}
+              center={[this.props.latlng[0][0], this.props.latlng[0][1]]}
+              zoom={14}
+            >
+              <TileLayer
+                url="https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              />
+              {this.props.latlng.length > 1 ? (
+                <Polygon positions={this.props.latlng} />
+              ) : (
+                <Marker position={this.props.latlng[0]} />
+              )}
+            </Map>
+          ) : null}
+        </Modal>
+      );
+    }
   }
 }
