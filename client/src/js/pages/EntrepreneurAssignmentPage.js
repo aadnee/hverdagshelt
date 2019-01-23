f5b1d01cad8a302a458c63e151a49b6ed3606296
Eivind Alfsvåg Johansen <eivindaj@stud.ntnu.no>
2019-01-23 17:12:07 +0100

    Company reject and accept functions made

--- client/src/js/pages/EntrepreneurAssignmentPage.js
+++ client/src/js/pages/EntrepreneurAssignmentPage.js
@@ -1,55 +1,83 @@
 import React from 'react';
-import {Component} from 'react';
-import {NavLink} from 'react-router-dom';
-import {Container, Grid, Header, Divider, Segment, Tab} from 'semantic-ui-react';
+import { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Container, Grid, Header, Divider, Segment, Tab } from 'semantic-ui-react';
 
 import { companyService } from '../services/CompanyServices';
 
-import {AssignmentWidget} from "../widgets/AssignmentWidget";
+import { AssignmentWidget } from '../widgets/AssignmentWidget';
 
 export class EntrepreneurAssignmentPage extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            assignments: []
-        };
-    }
+    this.handleDelete = this.handleDelete.bind(this);
 
-    componentWillMount() {
-        companyService.getTasks().then((res)=>{console.log(res);this.setState({assignments: res.res})});
-    }
+    this.state = {
+      assignments: []
+    };
+  }
 
-    render() {
-        let panes = [
-            {
-                menuItem: 'Oppdragsforespørsler',
-                render: () => (
-                    <Tab.Pane className="companyAssignmentTab frontPageFeedTab">
-                        {this.state.assignments.map((asg,i) => (<AssignmentWidget assignment={asg} newsOnly key={i}/>))}
-                    </Tab.Pane>
-                )
-            },
-            {
-                menuItem: 'Aktive oppdrag',
-                render: () => (
-                    <Tab.Pane className="companyActiveAssignmentTab frontPageFeedTab">
-                    <p>Hei</p>
-                    </Tab.Pane>
-                )
-            }
-        ];
-        return (
-            <>
-                <Divider hidden/>
-                <Divider hidden/>
-                <Container width={11} floated='true'>
-                    <Header as={'h1'}>Mine oppdrag</Header>
-                    <Segment basic color='blue'>
-                        <Tab menu={{text: true, secondary: true, pointing: true, color: 'blue'}} panes={panes}/>
-                    </Segment>
-                </Container>
-            </>
-        );
-    }
+  handleDelete = assigmentId => {
+    this.state.assignments.map((assigment, index) => {
+      if (assigment.id === assigmentId) {
+        let tempAssigments = this.state.assignments;
+        tempAssigments.splice(index, 1);
+        this.setState({ assignments: tempAssigments });
+      }
+    });
+  };
+
+  handleAccept = assigmentId => {
+    this.state.assignments.map((assigment, index) => {
+      if (assigment.id === assigmentId) {
+        let tempAssigments = this.state.assignments;
+        let asg = this.state.assignments[index];
+        tempAssigments.splice(index, 1, asg);
+        this.setState({ assignments: tempAssigments });
+      }
+    });
+  };
+
+  componentWillMount() {
+    companyService.getTasks().then(res => {
+      console.log(res);
+      this.setState({ assignments: res.res });
+    });
+  }
+
+  render() {
+    let panes = [
+      {
+        menuItem: 'Oppdragsforespørsler',
+        render: () => (
+          <Tab.Pane className="companyAssignmentTab frontPageFeedTab">
+            {this.state.assignments.map((asg, i) => (
+              <AssignmentWidget handleDelete={this.handleDelete.bind(this, asg.id)} assignment={asg} newsOnly key={i} />
+            ))}
+          </Tab.Pane>
+        )
+      },
+      {
+        menuItem: 'Aktive oppdrag',
+        render: () => (
+          <Tab.Pane className="companyActiveAssignmentTab frontPageFeedTab">
+            <p>Hei</p>
+          </Tab.Pane>
+        )
+      }
+    ];
+    return (
+      <>
+        <Divider hidden />
+        <Divider hidden />
+        <Container width={11} floated="true">
+          <Header as={'h1'}>Mine oppdrag</Header>
+          <Segment basic color="blue">
+            <Tab menu={{ text: true, secondary: true, pointing: true, color: 'blue' }} panes={panes} />
+          </Segment>
+        </Container>
+      </>
+    );
+  }
 }
