c23767a882c046253dba4cb5c91fda55eb5e5c77
Mathias Knutsen <mathiakn@stud.ntnu.no>
2019-01-24 10:23:12 +0100

    Login bug fix

--- server/src/server.js
+++ server/src/server.js
@@ -156,9 +156,11 @@ app.post('/api/news', ensureEmployee, (req, res) => {
 app.post('/api/login', (req, res) => {
   let b = req.body;
   userManager.login(b.email, b.password, function(result) {
-    res.cookie('token', result.token);
-    res.cookie('rank', result.rank);
-    res.cookie('municipalId', result.municipalId);
+    if (result.token) {
+      res.cookie('token', result.token);
+      res.cookie('rank', result.rank);
+      res.cookie('municipalId', result.municipalId);
+    }
     res.json(result);
   });
 });
